4dec2edd0a8b6de144636102d0138c71b3261fa2
Mukesh Mahato <157047963+MukeshMahato5138@users.noreply.github.com>
2024-09-05 10:25:29 +0000

    container config

---
+++ 01_basic/1_variables.js
@@ -0,0 +1,6 @@
+const accountId = 99893
+let accountEmail = "muku@555"
+var accountPassword = "5555"
+accountCity = "murtiya"
+
+console.table([accountId,accountEmail,accountPassword, accountCity]);

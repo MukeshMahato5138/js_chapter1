f736037d81dee6bc870000d61b15140e4d3bbbb0
Mukesh Mahato <157047963+MukeshMahato5138@users.noreply.github.com>
2024-09-05 10:38:24 +0000

    dataTypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; 
+//treat all old code as newer version
+
+let name = "mukesh"
+let age = 22
+let isLoggedIn = false
+// number = > 2 to power 52
+// BigInt
+//string=> "hellohellohellohellohellohellohello"
+
+//boolean => false/true
+// null => standalone value
+// undefine => 
+// symbol => unique
+
+// object 
+console.log(typeof "mukesh");
+console.log(typeof undefined); // 

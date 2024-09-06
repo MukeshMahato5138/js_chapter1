b4255cad5819a00b6d8d389be679d5ab813db7df
Mukesh Mahato <157047963+MukeshMahato5138@users.noreply.github.com>
2024-09-06 17:29:02 +0000

    .

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,10 @@
+// // console.log(2>1);
+// // console.log(2 >=1 );
+// // console.log( 2<1);
+// // console.log(2 == 1); // equal
+// // console.log(2!= 1);  // not equal sign
+
+// console.log("2" == 2);
+
+// console.log("2" === 2);
+

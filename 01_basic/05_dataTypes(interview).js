121698e9bc70e71835efa3cc546645972324c5a4
Mukesh Mahato <157047963+MukeshMahato5138@users.noreply.github.com>
2024-09-06 17:29:28 +0000

    interview

---
+++ 01_basic/05_dataTypes(interview).js
@@ -0,0 +1,25 @@
+// // # primitiveDataTypes
+// // 7types : string, Number, Boolean, Null, undefined, Symbol, BigInt
+
+
+// //const score = 100
+// /* JavaScript is a dynamic language and not static,
+//   which means that variables
+//  can hold values of different types during runtime*/
+ 
+
+
+// // ReferenceType (Non primitiveDataTypes) 
+// // Array, Objects, Functions
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId);
+
+
+
+ const bigNumber = 253115352162n
+ console.log(bigNumber);
+
+
+
+

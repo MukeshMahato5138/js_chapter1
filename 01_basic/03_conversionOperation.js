49e4f9f1bdcac7d32da29ffd4bf08f9daabbe21b
Mukesh Mahato <157047963+MukeshMahato5138@users.noreply.github.com>
2024-09-06 16:01:39 +0000

    update

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,21 +1,44 @@
-// let score = "66afgh"
-// console.log(typeof score);
+// // // let score = "66afgh"
+// // // console.log(typeof score);
 
 
-// let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// // "5" => 5
-// //"5df"=> NaN
-// true => 1 and false => 0
+// // // let valueInNumber = Number(score)
+// // // console.log(typeof valueInNumber);
+// // // // "5" => 5
+// // // //"5df"=> NaN
+// // // true => 1 and false => 0
 
-let isLoggedIn = ""
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// // let isLoggedIn = ""
+// // let booleanIsLoggedIn = Boolean(isLoggedIn)
+// // console.log(booleanIsLoggedIn);
 
 
-//" " false
-// "mukesh"  true
+// // //" " false
+// // // "mukesh"  true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// // let someNumber = 33
+// // let stringNumber = String(someNumber)
+// // console.log(typeof stringNumber);
+// // /// 
+
+// function func1(){
+//         return 2
+
+// }
+// function func2(){
+//         return 4
+// }
+
+// let a = (func1(), func2())
+
+// console.log(a);
+
+const arr = ["one", "two", "three"];
+const str = "mukesh";
+
+const res = arr.includes("on");
+const anotherVar = arr.includes("onetwo")
+const newVar = str.includes("11");
+
+console.table([res, anotherVar, newVar]);
+

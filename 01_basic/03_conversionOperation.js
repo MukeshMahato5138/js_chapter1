396c33b45a1ba515e0d5c0688745d7b919c76786
Mukesh Mahato <157047963+MukeshMahato5138@users.noreply.github.com>
2024-09-06 04:30:20 +0000

    conversion of data

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,21 @@
+// let score = "66afgh"
+// console.log(typeof score);
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// // "5" => 5
+// //"5df"=> NaN
+// true => 1 and false => 0
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+//" " false
+// "mukesh"  true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);

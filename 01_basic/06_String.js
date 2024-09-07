8996aa1440da9c5b0e2c5626fc10d71082fe5420
Mukesh Mahato <157047963+MukeshMahato5138@users.noreply.github.com>
2024-09-07 16:38:20 +0000

    string

---
+++ 01_basic/06_String.js
@@ -0,0 +1,23 @@
+// const name = "mukesh"
+// const repoCount = 10
+// /*+++++++++++  THIS IS THE OLD VERSION OF CONCATENATION ++++*/
+// console.log( "The final value is give here" + name + repoCount );
+
+// /*+++++++++++  THIS IS THE LEATEST VERSION OF CONCATENATION ++++
+
+// This is also know as string INTERPOLATION*/
+
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const userName = new String("Mukesh")
+
+console.log(userName[0]);
+console.log(userName.__proto__);
+console.log(userName.length);
+console.log(userName.toUpperCase());
+console.log(userName.charAt(2));
+console.log(userName.indexOf('s'));
+
+

82172835ec104703773bfe6d22bd6b2970cc8f10
Mukesh Mahato <157047963+MukeshMahato5138@users.noreply.github.com>
2024-09-08 14:50:07 +0000

    number_and_math

---
+++ 01_basic/07_num_math.js
@@ -0,0 +1,24 @@
+// const score = 400
+// const balance1 = new Number(100)
+// console.log(balance1);
+
+// const balance = new Number("mukes")
+// console.log(balance);
+// console.log(score);
+// console.log(balance1.toString().length);
+
+// console.log(balance1.toFixed(3));
+// const otherNumber = 223.8955
+// console.log(otherNumber.toPrecision(3));
+
+
+// const hundreds = 100000
+// console.log(hundreds. toLocaleString());
+// console.log(hundreds.valueOf());
+
+
+
+//++++++++++++++++++++++++MATHS +++++++++++++++++++
+
+console.log(Math);
+console.log(Math.sqrt());

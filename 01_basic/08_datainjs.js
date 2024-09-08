5bfa07b29fd3c46ff4760d07288f6dbb6ca252c6
Mukesh Mahato <157047963+MukeshMahato5138@users.noreply.github.com>
2024-09-08 16:16:43 +0000

    dateinjavascript

---
+++ 01_basic/08_datainjs.js
@@ -0,0 +1,29 @@
+// // DATEs
+
+// let myDate = new Date()
+// console.log(myDate)
+
+// console.log(myDate.toString());
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+
+
+console.log(typeof myDate);
+
+let myCreatedDate = new Date(2024,0,25)
+console.log(myCreatedDate.toDateString());
+
+
+
+
+
+
+
+
+
+
+// newDate.toLocalString('defult',{
+//     weekday: "long"
+// })
